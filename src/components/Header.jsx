cd74f8b9d1262aeaeb1ae2c4546e19fbe1b6be5d
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2023-12-18 23:23:37 +0530

    header is made responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../assets/images/DisneyLogo.jpeg'
-import { HiHome ,HiSearch,HiPlus,HiStar,HiOutlineDotsVertical, HiPlay} from "react-icons/hi";
+import { HiHome ,HiSearch,HiPlus,HiStar,HiDotsVertical, HiPlay} from "react-icons/hi";
 import { HiMiniTv } from "react-icons/hi2";
 import HeaderItems from './HeaderItems';
 
 
 function Header() {
+  const[toggle,setToggle]=useState(false);
   const menu=[
     {
        name:'HOME',
@@ -37,9 +38,27 @@ function Header() {
     <div className="flex items-center gap-10 justify-between  p-5">
       <div className='flex gap-8 items-center'>
       <img className="w-[80px] object-cover md:w-[115px] " src={logo}  />
+      <div className='hidden md:flex gap-8 '>
       {menu.map((item)=>(
          <HeaderItems name={item.name} Icon={item.icon}/>
       ) )}
+      </div>
+      <div className='flex md:hidden gap-5'>
+      {menu.map((item,index)=>index<3&&(
+         <HeaderItems name={''} Icon={item.icon}/>
+      ) )}
+      </div>
+      <div className=' md:hidden ' onClick={()=>setToggle(!toggle)}>
+      
+         <HeaderItems name={''} Icon={HiDotsVertical}/>
+         {toggle?<div className='absolute mt-3 bg-[#121212] border-[1px] p-3 border-gray-700 px-5 py-4'>
+      {menu.map((item,index)=>index>2&&(
+         <HeaderItems name={item.name} Icon={item.icon}/>
+      ) )}:
+      </div>:null}
+      
+      </div>
+
       </div>
       <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745" className='w-[40px] rounded-full' />
       
--- src/components/HeaderItems.jsx
+++ src/components/HeaderItems.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function HeaderItems({name,Icon}) {
   return (
     <div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline
-    underline-offset-8'>
+    underline-offset-8 mb-2'>
         <Icon/>
-        <h1>{name}</h1>
+        <h2 className=''>{name}</h2>
     </div>
   )
 }
